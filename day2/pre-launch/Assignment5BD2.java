b7504b5d2942f0dae9201c719a4a01d0107ff401
satheesh567 <chaitanyasugithi@gmail.com>
2024-03-06 14:44:32 +0530

    assignment 5b;

--- day2/pre-launch/Assignment5BD2.java
+++ day2/pre-launch/Assignment5BD2.java
@@ -1,27 +1,22 @@
 import java.util.Scanner;
-class Assignment5BD2{
- public static void main(String[] args){
-    
-        Scanner sc=new Scanner(System.in);
-         System.out.println("Enter value : ");
-         int rows=sc.nextInt();
-        for (int i = 0; i < rows; i++) {
-            
-            for (int j = 0; j < i; j++) {
-                System.out.print(" ");
-            }
-
-           
-            for (int k = 0; k < rows - i; k++) {
-                if (k % 2 == 0) {
-                    System.out.print("1");
-                } else {
-                    System.out.print("0");
-                }
-            }
-
-            System.out.println(); 
-        }
-    }
- 
+public class Assignment5BD2
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    System.out.println("Enter the number : ");
+	     int num=sc.nextInt();
+	     for(int i=0;i<num;i++){
+	         for(int j=0;j<i;j++){
+	         System.out.print(" ");
+	         }
+	         
+	         for(int j=1;j<=2*(num-i)-1;j++){
+	             if(j%2!=0)System.out.print("1");
+	             else
+	             System.out.print("0");
+	         }
+	         System.out.println();
+	     }
+		System.out.println("Hello World");
+	}
 }
